94ae85797540189b5cd12e0625e9b8fecf678288
Eklavya47 <eklavya5924@gmail.com>
2025-11-11 20:23:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/betanooblabs/mathquiz/Easy.kt
@@ -0,0 +1,30 @@
+package com.betanooblabs.mathquiz
+
+import kotlin.random.Random
+
+object Easy {
+
+    private var answer: Int = 0
+
+    fun getQuestions(): Pair<String, Int>{
+
+        val num1 = Random.nextInt(-100, 100)
+        val num2 = Random.nextInt(-100, 100)
+        val operator = arrayOf("+", "-", "*", "/").random()
+
+        val question = "$num1 $operator $num2"
+
+        answer = try {
+            when(operator){
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "*" -> num1 * num2
+                else -> num1 / num2
+            }
+        }catch (e: ArithmeticException){
+            777777
+        }
+
+        return Pair(question, answer)
+    }
+}
